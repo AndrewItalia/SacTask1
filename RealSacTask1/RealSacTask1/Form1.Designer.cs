effcb257c663ac517950f4cabebab063b848abe5
AndrewItalia <102033396+AndrewItalia@users.noreply.github.com>
2022-03-23 14:10:45 +1100

    Add files via upload

---
+++ RealSacTask1/RealSacTask1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RealSacTask1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numAge = new System.Windows.Forms.NumericUpDown();
+            this.numPurchasedPrice = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPurchasedPrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numAge
+            // 
+            this.numAge.Location = new System.Drawing.Point(88, 84);
+            this.numAge.Name = "numAge";
+            this.numAge.Size = new System.Drawing.Size(188, 26);
+            this.numAge.TabIndex = 0;
+            // 
+            // numPurchasedPrice
+            // 
+            this.numPurchasedPrice.DecimalPlaces = 1;
+            this.numPurchasedPrice.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numPurchasedPrice.Location = new System.Drawing.Point(88, 167);
+            this.numPurchasedPrice.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numPurchasedPrice.Name = "numPurchasedPrice";
+            this.numPurchasedPrice.Size = new System.Drawing.Size(188, 26);
+            this.numPurchasedPrice.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnCalculate.ForeColor = System.Drawing.Color.White;
+            this.btnCalculate.Location = new System.Drawing.Point(88, 238);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(125, 45);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(348, 238);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(134, 45);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "End The Quote";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "How old is the textbook (In years)?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "How much did you pay for it?";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(92, 340);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 28);
+            this.lblOutput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.numPurchasedPrice);
+            this.Controls.Add(this.numAge);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPurchasedPrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numAge;
+        private System.Windows.Forms.NumericUpDown numPurchasedPrice;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ RealSacTask1/RealSacTask1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RealSacTask1
+{
+    public partial class Form1 : Form
+    {
+        float total;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            float purchasedValue = (float)numPurchasedPrice.Value;
+            int age = (int)numAge.Value;
+            // if the age of amount spent is = 0 a error message will pop up 
+            if (purchasedValue == 0.0 | age == 0)
+            {
+                lblOutput.Text = "Please enter a valid price or year";
+            }
+            else
+            {
+                // calls functions to calculate the total
+                float currentValue = CalculateCurrentValue(purchasedValue, age);
+                total += currentValue;
+                lblOutput.Text = $"The item is worth ${currentValue}\nThe collection so far is worth ${total}";
+            }
+            resetInputs();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Calls the reset function
+            resetInputs();
+            lblOutput.Text = "";
+        }
+        private float CalculateCurrentValue(float purchasedValue, int age)
+        {
+            // Formula for depreciation the book will be worth 20% less as each year passes
+            float depreciation = purchasedValue * 0.2f * age;
+            // if depreciatation is greater than the purchase price then the value is 0
+            if (depreciation > purchasedValue) return 0f;
+            return purchasedValue - depreciation;
+        }
+
+
+        private void resetInputs()
+        {
+            // Creates Reset function when "End the quote" is pressed
+            // sets the age to 0 and the purchased price to 0
+            numAge.Value = 0;
+            numPurchasedPrice.Value = 0;
+        }
+
+
+    }
+}
